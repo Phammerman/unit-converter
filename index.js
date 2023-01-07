a97613f8bbd94445d1bc8b3849a551c8f05a340a
Phammerman <103281235+Phammerman@users.noreply.github.com>
2023-01-07 11:43:09 +0100

    Basic script
    Test out how to do the script

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const convertBtn = document.getElementById("convert-btn")
+const inputNumber = document.getElementById("input-number")
+
+
+
+convertBtn.addEventListener("click", function(){
+    console.log(inputNumber.value + " kg = " + inputNumber.value * 2.204 + " pound") 
+})
