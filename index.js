1b30049ac1a7d5ed47b0fa2474ac0292c2f34773
Phammerman <103281235+Phammerman@users.noreply.github.com>
2023-01-07 14:55:09 +0100

    TODO

--- index.js
+++ index.js
@@ -38,3 +38,23 @@ function convertValue(inpVal) {
 }
 
 convertValue(20)
+
+/*
+
+LOOK at this and make it easy
+
+let convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("input")
+let lengthEl = document.getElementById("length-el")
+
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
+
+convertBtn.addEventListener("click", function() {
+    let baseValue = inputEl.value
+    
+    lengthEl.textContent = `${baseValue} meter = ${baseValue * meterToFeet} feet` 
+})
+
+*/
