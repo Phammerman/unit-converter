92e7125e0e8c4237cc5cec12abb418a501a29ab7
Phammerman <103281235+Phammerman@users.noreply.github.com>
2023-01-07 12:46:52 +0100

    JS v.1.0 and started to style
    v.1.0 of the script is ready
    
    Started to style with css

--- index.js
+++ index.js
@@ -6,9 +6,34 @@
 
 const convertBtn = document.getElementById("convert-btn")
 const inputNumber = document.getElementById("input-number")
+const convertContainerOne = document.getElementById("convert-container-one")
+const convertContainerTwo = document.getElementById("convert-container-two")
+const convertContainerThree = document.getElementById("convert-container-three")
 
+console.log(convertContainerOne)
+const convertTable = {
+    convertToFeet: 3.281,
+    convertToGallon: 0.264,
+    convertToPound: 2.204
+}
+ 
 
 
 convertBtn.addEventListener("click", function(){
-    console.log(inputNumber.value + " kg = " + inputNumber.value * 2.204 + " pound") 
+    const inputValue = inputNumber.value
+    convertValue(inputValue)
+    console.log(convertedValues)
 })
+
+function convertValue(inpVal) {
+    convertedValues = [
+        inpVal * convertTable.convertToFeet,
+        inpVal * convertTable.convertToGallon,
+        inpVal * convertTable.convertToPound,
+    ]
+    
+    convertContainerOne.innerHTML = `<p> ${inpVal} meters = ${convertedValues[0]} feet </p> `
+    convertContainerTwo.innerHTML = `<p> ${inpVal} liters = ${convertedValues[1]} gallon </p> `
+    convertContainerThree.innerHTML = `<p> ${inpVal} kilogram = ${convertedValues[2]} pound</p> `
+}
+
