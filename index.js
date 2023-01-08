cde1b3abd510c7103f4028a47b7389a014e486cd
Phammerman <103281235+Phammerman@users.noreply.github.com>
2023-01-08 16:07:09 +0100

    Version 1.1 ready
    Javascript in fewer lines.
    Basic start up info added in HTML
    Some css styling is still left to do

--- index.js
+++ index.js
@@ -1,60 +1,34 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+
 
 const convertBtn = document.getElementById("convert-btn")
-const inputNumber = document.getElementById("input-number")
-const convertContainerOne = document.getElementById("convert-container-one")
-const convertContainerTwo = document.getElementById("convert-container-two")
-const convertContainerThree = document.getElementById("convert-container-three")
-const convertElOne = document.getElementById("convert-el-one")
+let inputEl = document.getElementById("input-el")
+console.log(inputEl)
 
-const convertTable = {
-    convertToFeet: 3.281,
-    convertToGallon: 0.264,
-    convertToPound: 2.204
-}
- 
-
-
-
-convertBtn.addEventListener("click", function(){
-    const inputValue = inputNumber.value
-    convertValue(inputValue)
-    console.log(convertedValues)
-})
-
-function convertValue(inpVal) {
-    convertedValues = [
-        inpVal * convertTable.convertToFeet,
-        inpVal * convertTable.convertToGallon,
-        inpVal * convertTable.convertToPound,
-    ]
-    
-    convertElOne.textContent = `${inpval} meter = ${convertedValues[0]}`
-    
-}
-
-convertValue(20)
-
-/*
-
-LOOK at this and make it easy
-
-let convertBtn = document.getElementById("convert-btn")
-let inputEl = document.getElementById("input")
-let lengthEl = document.getElementById("length-el")
+const lenghtEl = document.getElementById("lenght-el")
+const volumeEl = document.getElementById("volume-el")
+const weightEl = document.getElementById("weight-el")
 
 const meterToFeet =  3.281
 const literToGallon =  0.264
 const kiloToPound =  2.204
 
-convertBtn.addEventListener("click", function() {
+ 
+
+
+
+convertBtn.addEventListener("click", function(){
     let baseValue = inputEl.value
     
-    lengthEl.textContent = `${baseValue} meter = ${baseValue * meterToFeet} feet` 
+    if (isNaN(baseValue)) {
+        alert("Enter a number")
+    } else {
+        lenghtEl.textContent = `${baseValue} meter = ${(baseValue * meterToFeet).toFixed(3)} feet | ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(3)} meter`
+        volumeEl.textContent = `${baseValue} liter = ${(baseValue * literToGallon).toFixed(3)} galon | ${baseValue} galon = ${(baseValue / literToGallon).toFixed(3)} liter`
+        weightEl.textContent = `${baseValue} kilogram = ${(baseValue * kiloToPound).toFixed(3)} pound | ${baseValue} pound = ${(baseValue / kiloToPound).toFixed(3)} kilogram`
+    }
+
+
+
+
 })
 
-*/
